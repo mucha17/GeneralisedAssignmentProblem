107f3aad6131104d88cd8266e1baa35f94684637
mucha17 <46112158+mucha17@users.noreply.github.com>
2021-02-07 19:10:33 +0100

    Add GreedyFit for initial solution generation

---
+++ src/pl/umg/Algorithms/Fitness/GreedyFit.java
@@ -0,0 +1,56 @@
+package pl.umg.Algorithms.Fitness;
+
+import pl.umg.Agent;
+import pl.umg.Job;
+import pl.umg.Problem;
+
+import java.util.Arrays;
+
+public class GreedyFit {
+
+	private static final double COST_VALUE = 1;
+	private static final double RESOURCE_VALUE = 1;
+
+	public static void assignJobsToAgents(Problem problem) {
+		boolean[] jobsAssigned = new boolean[problem.getNumberOfJobs()]; // automatically has false values
+		Boolean[] canBeAssignedAnotherJob = new Boolean[problem.getNumberOfAgents()]; // has to be assigned true values
+		Arrays.fill(canBeAssignedAnotherJob, true);
+		while (Arrays.asList(canBeAssignedAnotherJob).contains(true)) {
+			for (int agentIndex : problem.getAgents().keySet()) {
+				Agent agent = problem.getAgents().get(agentIndex);
+				Job bestJob = null;
+				canBeAssignedAnotherJob[agentIndex] = false; // set to false so it can be checked against jobs
+				for (int jobIndex : problem.getJobs().keySet()) {
+					if (!jobsAssigned[jobIndex]) {
+						Job job = problem.getJobs().get(jobIndex);
+						if (agent.isAssignable(job.getResourcesConsumedInAllocating().get(agent))) {
+							canBeAssignedAnotherJob[agentIndex] = true;
+						}
+						int resource = job.getResourcesConsumedInAllocating().get(agent);
+						int cost = job.getCostsOfAllocating().get(agent);
+						if (bestJob == null) {
+							bestJob = job;
+						} else {
+							int bjresource = bestJob.getResourcesConsumedInAllocating().get(agent);
+							int bjcost = bestJob.getCostsOfAllocating().get(agent);
+							if ((COST_VALUE * cost + RESOURCE_VALUE * resource) < (COST_VALUE * bjcost + RESOURCE_VALUE * bjresource)) {
+								bestJob = job;
+							}
+						}
+					}
+				}
+				if (bestJob != null && agent.addAssignment(bestJob)) {
+					Integer jobIndex = null;
+					for (int key : problem.getJobs().keySet()) {
+						if (problem.getJobs().get(key).equals(bestJob)) {
+							jobIndex = key;
+						}
+					}
+					if (jobIndex != null) {
+						jobsAssigned[jobIndex] = true;
+					}
+				}
+			}
+		}
+	}
+}
--- src/pl/umg/Main.java
+++ src/pl/umg/Main.java
@@ -1,5 +1,6 @@
 package pl.umg;
 
+import pl.umg.Algorithms.Fitness.GreedyFit;
 import pl.umg.Utils.TestDataReader;
 
 import java.util.Map;
@@ -12,6 +13,13 @@ public class Main {
 		final int file = 1; // in range 1..12
 		Map<Integer, Problem> problems = TestDataReader.readProblemsFromFile("resources/gap" + file + ".txt");
 
+		Problem problem = problems.get(0);
+
+		GreedyFit.assignJobsToAgents(problem);
+
+		for(int key : problem.getAgents().keySet()) {
+			System.out.println("Agent " + key + " has " + problem.getAgents().get(key).getAssignments().size() + " assignments");
+		}
 
 	}
 }
